96ff16d74244069d3a3bce417b4908efbe83378f
Martino Thomann <martino.thomann@gmail.com>
2026-01-10 22:05:06 +0100

    remove special return keyword highlighting

--- colors/matho.lua
+++ colors/matho.lua
@@ -7,7 +7,7 @@ local textPalette = {
     delimiter = "#7c7c7c",
     variable = "#9e9e9e",
     functionDef = "#70a7cf",
-    exit = "#af5993",
+    exit = "#bbbbbb",
 
     character = "#ad858f",
     number = "#9285ad",
